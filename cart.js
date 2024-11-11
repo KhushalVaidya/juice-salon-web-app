852f7a9d90cd63b5be44431942519c02d231d7f7
KhushalVaidya <khushalvaidya1920@gmail.com>
2024-11-11 15:17:22 +0530

    Project Added

---
+++ cart.js
@@ -0,0 +1,138 @@
+
+const CONVENIENCE_FEES=99;
+let bagItemObjects;
+
+
+onLoad();
+function onLoad(){
+    loadBagItemObjects();
+   
+    displayBagItems();
+    displayBagSummary();
+    
+    
+}
+
+
+function displayBagSummary(){
+    let bagSummaryElement = document.querySelector('.bag-summary');
+let totalItems=bagItemObjects.length;
+let totalMrp=0;
+let totalDiscount=0;
+
+bagItemObjects.forEach(bagItem=>{
+    totalMrp += bagItem.original_price;
+    totalDiscount += bagItem.original_price-bagItem.current_price;
+})
+
+let finalPayment;
+// if(bagItemObjects.length>0){
+//     finalPayment=totalMrp-totalDiscount+ CONVENIENCE_FEES;
+// }
+// else{
+//     finalPayment=0;
+// }
+{(bagItemObjects.length>0 ? finalPayment=totalMrp-totalDiscount+ CONVENIENCE_FEES :finalPayment=0)};
+
+
+
+
+    bagSummaryElement.innerHTML =` <div class="bag-details-container">
+            <div class="price-header">PRICE DETAILS (${totalItems} Items) </div>
+            <div class="price-item">
+              <span class="price-item-tag">Total MRP</span>
+              <span class="price-item-value">Rs${totalMrp}</span>
+            </div>
+            <div class="price-item">
+              <span class="price-item-tag">Discount on MRP</span>
+              <span class="price-item-value priceDetail-base-discount">-Rs${totalDiscount}</span>
+            </div>
+            <div class="price-item">
+              <span class="price-item-tag">Convenience Fee</span>
+              <span class="price-item-value">${(bagItemObjects.length>0 ?  CONVENIENCE_FEES :0)}</span>
+            </div>
+            <hr>
+            <div class="price-footer">
+              <span class="price-item-tag">Total Amount</span>
+              <span class="price-item-value">Rs${finalPayment}</span>
+            </div>
+          </div>
+          <button class="btn-place-order">
+            <div class="css-xjhrni" onclick="placeOrder()">PLACE ORDER</div>
+          </button>`;
+}
+
+
+
+
+function loadBagItemObjects(){
+  console.log(bagItems);
+  bagItemObjects =bagItems.map(itemId =>{
+    for(let i=0 ; i<items.length ; i++){
+      if(itemId == items[i].id){
+        return items[i];
+      }
+    }})
+  }
+
+ 
+  
+  console.log(bagItemObjects);
+
+  
+  function placeOrder(){
+  
+  if(bagItemObjects.length>0){
+    alert(`Your Order has been placed Successfully`);
+  }else{
+    alert('You Have not Placed Anything');
+  
+  }
+  }
+  
+function displayBagItems(){
+
+    let containerElement=document.querySelector('.bag-items-container');
+    let innerHTML='';
+    bagItemObjects.forEach(bagItem =>{
+        innerHTML+=generateItemHtml(bagItem);
+    });
+    containerElement.innerHTML=innerHTML;
+}
+
+
+
+function removeFromBag(itemId){
+    bagItems=bagItems.filter(bagItemId => bagItemId !=itemId)
+    localStorage.setItem('bagItems',JSON.stringify(bagItems));
+    loadBagItemObjects();
+    displayBagIcon();
+    displayBagItems(); 
+    displayBagSummary();
+}
+
+function generateItemHtml(item){
+return `<div class="bag-item-container">
+            <div class="item-left-part">
+              <img class="bag-item-img" src="${item.image}">
+            </div>
+            <div class="item-right-part">
+              <div class="company">${item.company}</div>
+              <div class="item-name">${item.item_name}</div>
+              <div class="price-container">
+                <span class="current-price">${item.current_price}</span>
+                <span class="original-price">${item.original_price}</span>
+                <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
+              </div>
+              <div class="return-period">
+                <span class="return-period-days">${item.return_period} days</span> return available
+              </div>
+              <div class="delivery-details">
+                Delivery by
+                <span class="delivery-details-days">${item.delivery_date}</span>
+              </div>
+            </div>
+
+            <div class="remove-from-cart" onclick="removeFromBag(${item.id})">X</div>
+          </div>`
+}
---
+++ items.js
@@ -0,0 +1,128 @@
+const items = [
+    {
+        id: '001',
+        image:'id001image.jpeg',
+        company: 'Kérastase Chroma Absolu Bain ',
+        item_name: 'Riche Chroma Shampoo 250ml',
+        original_price: 2650,
+        current_price: 1113,
+        discount_percentage: 42,
+        return_period: 14,
+        delivery_date: '10 Oct 2024',
+        rating: {
+            stars: 4.5,
+            count: 1400,
+        },
+    },
+    {
+        id: '002',
+        image: '002id.jpg',
+        company: 'Kérastase Chroma Absolu Bain ',
+        item_name: 'Chroma Respect Shampoo 250ml',
+        original_price: 2599,
+        current_price: 1507,
+        discount_percentage: 42,
+        return_period: 14,
+        delivery_date: '10 Oct 2024',
+        rating: {
+            stars: 4.3,
+            count: 24,
+        },
+    },
+    {
+        id: '003',
+        image: 'id003image.jpeg',
+        company: 'Kérastase Chroma Absolu Bain',
+        item_name: 'Vital Dermo Calm 250 ML',
+        original_price: 1599,
+        current_price: 495,
+        discount_percentage: 69,
+        return_period: 14,
+        delivery_date: '10 Oct 2024',
+        rating: {
+            stars: 4.1,
+            count: 249,
+        },
+    },
+
+    {
+        id: '004',
+        image: 'id004image.jpeg',
+        company: 'Kérastase Chroma Absolu Bain',
+            item_name: ' Homme 250ML D47',
+            original_price: 999,
+            current_price: 999,
+            discount_percentage: 0,
+            return_period: 14,
+            delivery_date: '10 Oct 2024',
+            rating: {
+                stars: 5.0,
+                count: 10,
+            },
+        },
+    {
+        id: '005',
+        image: '002id.jpg',
+        company: 'Kérastase Chroma Absolu Bain ',
+        item_name: 'Chroma Respect Shampoo 250ml',
+        original_price: 2599,
+        current_price: 1507,
+        discount_percentage: 42,
+        return_period: 14,
+        delivery_date: '10 Oct 2024',
+        rating: {
+            stars: 4.3,
+            count: 24,
+        },
+    },
+    {
+        id: '006',
+        image: '002id.jpg',
+        company: 'Kérastase Chroma Absolu Bain ',
+        item_name: 'Chroma Respect Shampoo 250ml',
+        original_price: 2599,
+        current_price: 1507,
+        discount_percentage: 42,
+        return_period: 14,
+        delivery_date: '10 Oct 2024',
+        rating: {
+            stars: 4.3,
+            count: 24,
+        },
+    },
+ 
+   
+   
+    {
+        id: '007',
+        image: 'id003image.jpeg',
+        company: 'L’Oréal Professionnel ',
+        item_name: 'Hair Color Pure Shampoo 250 ml',
+        original_price: 285,
+        current_price: 142,
+        discount_percentage: 50,
+        return_period: 14,
+        delivery_date: '10 Oct 2024',
+        rating: {
+            stars: 4.2,
+            count: 5200,
+        },
+    },
+
+    {
+        id: '008',
+        image: 'id004image.jpeg',
+        company: 'L’Oréal Professionnel ',
+        item_name: 'AC Anti-Dandruff Shampoo 250ml',
+        original_price: 1599,
+        current_price: 639,
+        discount_percentage: 60,
+        return_period: 14,
+        delivery_date: '10 Oct 2024',
+        rating: {
+            stars: 4.2,
+            count: 388,
+        },
+    },
+   
+];
